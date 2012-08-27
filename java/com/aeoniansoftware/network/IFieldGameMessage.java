6faa1b8876b40a1cf777a2657deb1e56ef63da5f
Fred Skallos <fskallos@aeoniansoftware.com>
2012-08-27 16:38:10 -0400

    Daily commit 8-27-12

--- java/com/aeoniansoftware/network/IFieldGameMessage.java
+++ java/com/aeoniansoftware/network/IFieldGameMessage.java
@@ -16,7 +16,7 @@ import com.jme3.network.Message;
  * @author kg
  */
 public interface IFieldGameMessage 
-extends Comparable<IFieldGameMessage> {
+extends Comparable<IFieldGameMessage>, Message{
     
     public static final Comparator<IFieldGameMessage> Compare = 
     new Comparator<IFieldGameMessage>() {
